18650362d83a9e76aa62f4b16444a4d7c384fbcc
AlucardQuevedo <mr.quevedo@yahoo.com>
2026-04-22 17:20:59 +0000

    Worked on Logic for Choosing and Added Comments
    Co-authored-by: imnottlarry <imnottlarry@users.noreply.github.com>
    Co-authored-by: mrperez103-dev <mrperez103-dev@users.noreply.github.com>

--- main.py
+++ main.py
@@ -22,6 +22,13 @@ def changeGrid (hiddenGrids,difficultie):
         indexCount = indexCount + 1
     return hiddenGrids
 
+def check (hiddenGridss, rows, columns):
+    rows = (rows -1)*5
+    indexOfChoose=rows+columns-1
+    if hiddenGridss[indexOfChoose]=="$":
+        return True
+    if hiddenGridss[indexOfChoose]=="💣":
+        return False
 
 
 #Telling Instructions
@@ -43,16 +50,25 @@ amountWager= int(input("How much do you want to wager: "))
 difficulty= int(input("How difficult do you want it 1-20?"))
 difficultyForMult = difficulty * 0.25
 multiplier=1.1+difficultyForMult
-
 hiddenGridFR= changeGrid (hiddenGrid,difficulty)
 
-print (hiddenGridFR)
 
 
-choice1=input ("Do you want to do choose or random: ")
+for i in range (0,25,5): #goes (start,stop,step)
+    print(hiddenGridFR[i:i+5]) #go from 0->6 but doesn't do 6 only 5
 
-if choice1=="choose":
-    row=int(input("Please pick a row: "))
-    column=int(input("Please pick a column: "))
+#underContruction
+goAgain="Y"
+while goAgain=="Y":
+    choice1=input ("Do you want to do choose or random: ")
+
+    if choice1=="choose":
+        row=int(input("Please pick a row: "))
+        column=int(input("Please pick a column: "))
+        if check(hiddenGridFR, row, column) == True:
+            amountWager= amountWager * multiplier
+            
+    
+
 
 
