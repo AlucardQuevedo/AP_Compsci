d16dbaa398cb70f78e1ab2d5ce02c57ff3e1faf0
AlucardQuevedo <mr.quevedo@yahoo.com>
2026-04-23 05:55:29 +0000

    Worked on Choose Section and Has error with only being able to do it once

--- main.py
+++ main.py
@@ -30,7 +30,16 @@ def check (hiddenGridss, rows, columns):
     if hiddenGridss[indexOfChoose]=="💣":
         return False
 
+def updateGrid (hiddenGridsss, shownGridsss, rowss, columnss):
+    rowss= (rowss-1) *5 
+    indexOfCHOOSE=rowss+columnss-1
+    shownGridsss[indexOfCHOOSE]=hiddenGridsss[indexOfCHOOSE]
+    return shownGridsss
 
+
+def displayGrid(gridDisplayed):
+    for i in range (0,25,5): #goes (start,stop,step)
+        print(gridDisplayed[i:i+5]) #go from 0->6 but doesn't do 6 only 5
 #Telling Instructions
 
 # EXAMPLE
@@ -41,9 +50,6 @@ print ("pick a bomb and you lose money")
 print ("pick a dollar and you win money")
 print ("You have 100$ to start")
 
-for i in range (0,25,5): #goes (start,stop,step)
-    print(shownGrid[i:i+5]) #go from 0->6 but doesn't do 6 only 5
-
 
 #Asking For Wager and Diff 
 Money=100
@@ -53,27 +59,34 @@ difficulty= int(input("How difficult do you want it 1-20?"))
 difficultyForMult = difficulty * 0.25
 multiplier=1.1+difficultyForMult
 hiddenGridFR= changeGrid (hiddenGrid,difficulty)
+#testing displayGrid(hiddenGridFR)
 
-
-
-for i in range (0,25,5): #goes (start,stop,step)
-    print(hiddenGridFR[i:i+5]) #go from 0->6 but doesn't do 6 only 5
 
 #underContruction
 goAgain="Y"
-while goAgain=="Y":
-    choice1=input ("Do you want to do choose or random: ")
 
-    if choice1=="choose":
+choice1=input ("Do you want to do choose or random: ")
+
+if choice1=="choose":
+    while goAgain=="Y":
+        displayGrid(shownGrid)
         row=int(input("Please pick a row: "))
         column=int(input("Please pick a column: "))
+
         if check(hiddenGridFR, row, column) == True:
             amountWager= amountWager * multiplier
             print("You earned $", amountWager,)
-            goAgain==input("Do you want to keep going (Y/N)")
-        if check(hiddenGridFR, row, column == False):
-            print ("You blew up...")
+            shownGrid=updateGrid (hiddenGridFR, shownGrid, row, column)
+            displayGrid(shownGrid)
+            goAgain==input("Do you want to keep going (Y/N): ")
+                
+
+        elif check(hiddenGridFR, row, column == False):
+            print ("\n\n\nYou blew up...")
             print ("You have now only have $", Money)
+            displayGrid (hiddenGridFR)
+            goAgain="N"
+
         
             
             
